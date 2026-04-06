24c63a00e9c548030634569b1bf4316eadd40155
Kazeem <W22075214@northumbria.ac.uk>
2026-04-06 18:08:47 +0100

    Updated the service screen

--- src/screens/Index/ServicesScreen.jsx
+++ src/screens/Index/ServicesScreen.jsx
@@ -40,34 +40,36 @@ function ServicesScreen() {
 
         <div className="features">
           <h2>Features</h2>
-          <div className="feature-box box-1">
-            <h3>Buying and Selling Made Easy</h3>
-            <p>
-              At Student E-Commerce Marketplace, buying and selling is
-              streamlined for busy college students. Easily list your unused
-              items like textbooks or gadgets with quick photo uploads,
-              descriptions, and pricing.
-            </p>
-          </div>
+          <div className="feature-contents">
+            <div className="feature-box box-1">
+              <h3>Buying and Selling Made Easy</h3>
+              <p>
+                At Student E-Commerce Marketplace, buying and selling is
+                streamlined for busy college students. Easily list your unused
+                items like textbooks or gadgets with quick photo uploads,
+                descriptions, and pricing.
+              </p>
+            </div>
 
-          <div className="feature-box box-2">
-            <h3>Community and Engagement Features</h3>
-            <p>
-              Foster a vibrant campus community with built-in tools for
-              interaction and discovery. Our features include real-time in-app
-              chat for haggling and questions, instant notifications for new
-              listings or messages, user ratings and reviews to ensure trust.
-            </p>
-          </div>
+            <div className="feature-box box-2">
+              <h3>Community and Engagement Features</h3>
+              <p>
+                Foster a vibrant campus community with built-in tools for
+                interaction and discovery. Our features include real-time in-app
+                chat for haggling and questions, instant notifications for new
+                listings or messages, user ratings and reviews to ensure trust.
+              </p>
+            </div>
 
-          <div className="feature-box box-3">
-            <h3>Safety and Verification Services</h3>
-            <p>
-              At Student E-Commerce Marketplace, your safety is our top
-              priority. We use school email verification to ensure only genuine
-              students join the platform, creating a trusted campus-only
-              community.
-            </p>
+            <div className="feature-box box-3">
+              <h3>Safety and Verification Services</h3>
+              <p>
+                At Student E-Commerce Marketplace, your safety is our top
+                priority. We use school email verification to ensure only
+                genuine students join the platform, creating a trusted
+                campus-only community.
+              </p>
+            </div>
           </div>
         </div>
       </section>
